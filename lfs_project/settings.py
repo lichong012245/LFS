e5aeca1807c40fa93e02bb0330c1c1905d3f43c9
John Li <inventu.tyrone.lee@gmail.com>
2014-06-04 18:08:55 -0400

    remove static_dir from setting

--- lfs_project/settings.py
+++ lfs_project/settings.py
@@ -325,9 +325,9 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 try:
     from local_settings import *
