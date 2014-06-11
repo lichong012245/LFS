6287a6639bf64974c1e7550e36fa47f7c04ffbf4
John Li <inventu.tyrone.lee@gmail.com>
2014-06-11 19:51:55 -0400

    copy migration from development trunk to current project:lfs.portlet

--- lfs_project/lfs/portlet/migrations/0001_initial.py
+++ lfs_project/lfs/portlet/migrations/0001_initial.py
@@ -1,13 +1,13 @@
-# encoding: utf-8
+# -*- coding: utf-8 -*-
 import datetime
 from south.db import db
 from south.v2 import SchemaMigration
 from django.db import models
 
+
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        
         # Adding model 'AverageRatingPortlet'
         db.create_table('portlet_averageratingportlet', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
@@ -81,7 +81,6 @@ class Migration(SchemaMigration):
             ('title', self.gf('django.db.models.fields.CharField')(max_length=100, blank=True)),
             ('show_product_filters', self.gf('django.db.models.fields.BooleanField')(default=True)),
             ('show_price_filters', self.gf('django.db.models.fields.BooleanField')(default=True)),
-            ('show_manufacturer_filters', self.gf('django.db.models.fields.BooleanField')(default=False)),
         ))
         db.send_create_signal('portlet', ['FilterPortlet'])
 
@@ -105,19 +104,8 @@ class Migration(SchemaMigration):
         ))
         db.send_create_signal('portlet', ['FeaturedPortlet'])
 
-        # Adding model 'LatestPortlet'
-        db.create_table('portlet_latestportlet', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=100, blank=True)),
-            ('limit', self.gf('django.db.models.fields.IntegerField')(default=5)),
-            ('current_category', self.gf('django.db.models.fields.BooleanField')(default=False)),
-            ('slideshow', self.gf('django.db.models.fields.BooleanField')(default=False)),
-        ))
-        db.send_create_signal('portlet', ['LatestPortlet'])
-
 
     def backwards(self, orm):
-        
         # Deleting model 'AverageRatingPortlet'
         db.delete_table('portlet_averageratingportlet')
 
@@ -154,9 +142,6 @@ class Migration(SchemaMigration):
         # Deleting model 'FeaturedPortlet'
         db.delete_table('portlet_featuredportlet')
 
-        # Deleting model 'LatestPortlet'
-        db.delete_table('portlet_latestportlet')
-
 
     models = {
         'portlet.averageratingportlet': {
@@ -192,21 +177,12 @@ class Migration(SchemaMigration):
         'portlet.filterportlet': {
             'Meta': {'object_name': 'FilterPortlet'},
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'show_manufacturer_filters': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'show_price_filters': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'show_product_filters': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'title': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'})
         },
         'portlet.forsaleportlet': {
             'Meta': {'object_name': 'ForsalePortlet'},
-            'current_category': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'limit': ('django.db.models.fields.IntegerField', [], {'default': '5'}),
-            'slideshow': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'})
-        },
-        'portlet.latestportlet': {
-            'Meta': {'object_name': 'LatestPortlet'},
             'current_category': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'limit': ('django.db.models.fields.IntegerField', [], {'default': '5'}),
