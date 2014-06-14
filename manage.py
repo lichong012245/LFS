ed5fe10cd5e29e731cae7f20a6b0ebb6f6366e61
John Li <inventu.tyrone.lee@gmail.com>
2014-06-14 16:46:06 -0700

    merge from heroku

--- manage.py
+++ manage.py
@@ -2,7 +2,11 @@
 import os
 import sys
 
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
 if __name__ == "__main__":
+
+    
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lfs_project.settings")
 
     from django.core.management import execute_from_command_line
