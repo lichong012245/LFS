1349685022fbafa7833d015ef722fa537fe0e594
John Li <inventu.tyrone.lee@gmail.com>
2014-07-03 16:36:58 -0400

    seach result list design finished and added.

--- lfs_project/settings.py
+++ lfs_project/settings.py
@@ -101,6 +101,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.transaction.TransactionMiddleware',
     'lfs.utils.middleware.RedirectFallbackMiddleware',
     "pagination.middleware.PaginationMiddleware",
 )
