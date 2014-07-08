eb3c41e19071752b4692a6859189be8b1d5f346a
John Li <inventu.tyrone.lee@gmail.com>
2014-07-08 15:35:00 -0400

    fix cache conflict for topsellerall

--- lfs_project/portlet/models/topsellerall.py
+++ lfs_project/portlet/models/topsellerall.py
@@ -3,6 +3,8 @@ from django import forms
 from django.db import models
 from django.template import RequestContext
 from django.template.loader import render_to_string
+from django.db.models import Q
+from django.db import connection
 
 # portlets imports
 from portlets.models import Portlet
@@ -11,6 +13,8 @@ from portlets.models import Portlet
 import lfs.catalog.utils
 import lfs.catalog.models
 import lfs.marketing.utils
+from lfs.catalog.models import Product
+from lfs.marketing.models import Topseller
 
 
 
@@ -29,16 +33,17 @@ class TopsellerPortletAll(Portlet):
         """Renders the portlet as html.
         """
         request = context.get("request")
-        object = context.get("category") or context.get("product")
-        if object is None:
-            topseller = lfs.marketing.utils.get_topseller(self.limit)
-        elif isinstance(object, lfs.catalog.models.Product):
-            category = object.get_current_category(context.get("request"))
-            topseller = lfs.marketing.utils.get_topseller_for_category(
-                category, self.limit)
-        else:
-            topseller = lfs.marketing.utils.get_topseller_for_category(
-                object, self.limit)
+        # object = context.get("category") or context.get("product")
+        # if object is None:
+        #     topseller = lfs.marketing.utils.get_topseller(self.limit)
+        # elif isinstance(object, lfs.catalog.models.Product):
+        #     category = object.get_current_category(context.get("request"))
+        #     topseller = lfs.marketing.utils.get_topseller_for_category(
+        #         category, self.limit)
+        # else:
+        #     topseller = lfs.marketing.utils.get_topseller_for_category(
+        #         object, self.limit)
+        topseller = self.get_topseller()
 
         return render_to_string("lfs/portlets/topseller.html", RequestContext(request, {
             "title": self.title,
@@ -47,6 +52,40 @@ class TopsellerPortletAll(Portlet):
 
     def form(self, **kwargs):
         return TopsellerAllForm(instance=self, **kwargs)
+
+    def get_topseller(self):
+        """Returns all products with the most sales."""    
+
+    # TODO: Check Django 1.1's aggregation
+        cursor = connection.cursor()
+        cursor.execute("""SELECT product_id, sum(product_amount) as sum
+                      FROM order_orderitem
+                      where product_id is not null
+                      GROUP BY product_id
+                      ORDER BY sum DESC""")
+
+        products = []
+        for topseller in cursor.fetchall():
+            product = Product.objects.get(pk=topseller[0])
+            if product.is_active():
+                try:
+                    products.append(product)
+                except Product.DoesNotExist:
+                    pass
+
+        for explicit_ts in Topseller.objects.all():
+
+            if explicit_ts.product.is_active():
+            # Remove explicit_ts if it's already in the object list
+                if explicit_ts.product in products:
+                    products.pop(products.index(explicit_ts.product))
+
+            # Then reinsert the explicit_ts on the given position
+                position = explicit_ts.position - 1
+                if position < 0:
+                    position = 0
+                products.insert(position, explicit_ts.product)        
+        return products
 
 
 class TopsellerAllForm(forms.ModelForm):
